64d884634a5b856048b91882892a2af5e0049fd6
青竹 <50625422+dfwzr@users.noreply.github.com>
2020-12-10 21:30:22 +0800

    Update setlink.sh

--- setlink.sh
+++ setlink.sh
@@ -1,9 +1,9 @@
-base64 list > /home/wwwroot/web_camouflage/test
+base64 list > /home/wwwroot/web_camouflage/abc
 echo "订阅已生成"
 for((i=30;i>0;i=i-5));
 do
 	echo "$i秒后关闭订阅";
 	sleep 5s;
 done
-rm /home/wwwroot/web_camouflage/test
+rm /home/wwwroot/web_camouflage/abc
 echo "订阅已删除"
